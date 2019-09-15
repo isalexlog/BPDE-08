f04b35b59f4354ec8e012b75e00c7e45cd069750
Ormolu <ormolu@web.de>
2019-09-15 16:16:17 +0200

    homework from 2019-09-15 - log errors

--- 2019-09-13_Static_fields_and_methods/src/Main.java
+++ 2019-09-13_Static_fields_and_methods/src/Main.java
@@ -7,10 +7,28 @@ public class Main {
     public static void main(String[] args) {
         Logger log = Logger.getLogger(Main.class);
 
-        log.info(CircleUtils.PI);
-        log.info(CircleUtils.circleLength(5));
-        log.info(CircleUtils.circleArea(5));
-        log.info(CircleUtils.squareEquivalent(1));
-        log.info(Math.sqrt(CircleUtils.PI));
+        log.info("" + CircleUtils.PI);
+
+        double length = CircleUtils.circleLength(-5);
+        double area = CircleUtils.circleArea(5);
+        double quadrature = CircleUtils.squareEquivalent(1);
+
+        if (length < 0)
+            log.error("Error while calculating circle length.");
+        else
+            log.info("Circle length = " + length);
+
+        if (area < 0)
+            log.error("Error while calculating circle area.");
+        else
+            log.info("Circle area = " + area);
+
+        if(quadrature < 0)
+            log.error("Error while calculating circle quadrature.");
+        else
+            log.info("Circle quadrature = " + quadrature);
+
+        log.info("" + Math.sqrt(CircleUtils.PI));
+
     }
 }
