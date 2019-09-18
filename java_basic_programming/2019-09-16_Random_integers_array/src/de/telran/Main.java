1a52d6c401ef4eb5288c50a36c2131681ec07fee
lesyaAn <lisaalisa92@bk.ru>
2019-09-18 14:20:55 +0200

    Home Work 2019_09_18

---
+++ java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
@@ -0,0 +1,28 @@
+package de.telran;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[20];
+        int low = 10;
+        int high = 100;
+
+
+        for(int i = 0; i < a.length; i++){
+            a[i] = getRandom(low, high);
+            System.out.println(Arrays.toString(a));
+
+        }
+
+    }
+
+    public static int getRandom (int low, int high){
+        Random r = new Random();
+        return r.nextInt(high-low) + low;
+    }
+
+
+}
