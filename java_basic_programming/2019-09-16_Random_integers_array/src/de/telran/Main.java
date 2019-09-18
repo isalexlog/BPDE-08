92ff5a389c06c5b607d4b4be7c1dfbb1864242d3
pilyav4ik <ip.artweb@yahoo.de>
2019-09-18 17:38:50 +0200

    HW: Random_integers_array completed

--- java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
+++ java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
@@ -1,8 +1,27 @@
 package de.telran;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Random rand = new Random();
+        int a, b;
+        int c = 100;
+        var randNum = new HashSet<Integer>();
+
+        for (b = 0; b < c; b++) {
+            a = rand.nextInt(100);
+            randNum.add(a);
+            if (randNum.size() <= 10) {
+                if (randNum.size() == 10) {
+                    c = 10;
+                }
+                c++;
+                randNum.add(a);
+            }
+        }
+        System.out.println("Ten random numbers: " + randNum);
     }
 }
