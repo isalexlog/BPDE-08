5bb0b53a810bc4258b35f5c069f535d83bf7bdbf
lesyaAn <lisaalisa92@bk.ru>
2019-09-18 12:15:50 +0200

    sample commit

--- java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
+++ java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //new comment just to add something to commit
+        //new comment just to add something to commit 2
 
     }
 }
