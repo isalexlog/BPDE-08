86e516497709dbbce0a2d853c4e0f93ce61878bf
Your NameNailyaD <nailya.doskanova@outlook.com>
2019-09-18 16:42:07 +0600

    HW

--- java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
+++ java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
@@ -1,8 +1,23 @@
 package de.telran;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] arrayOfInt = new int[20];
+        for (int i = 0; i < arrayOfInt.length; i++) {
+            arrayOfInt[i] = randomNumber(100, 200);
+        }
+        System.out.println(Arrays.toString(arrayOfInt));
+    }
 
+    public static int randomNumber(int a, int b) {
+        int diff = b - a;
+        Random random = new Random();
+        int c = random.nextInt(diff + 1);
+        c += a;
+        return c;
     }
 }
