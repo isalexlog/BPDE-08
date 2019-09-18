78b5224585a6e1fb2ca855ec98d83292615675b0
AlexK <alexkagan1985@gmail.com>
2019-09-18 15:59:56 +0200

    HomeWork 2019-09-18

---
+++ java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
@@ -0,0 +1,10 @@
+package de.telran;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MathOperations.operator(1,100);
+
+    }
+}
---
+++ java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/MathOperations.java
@@ -0,0 +1,37 @@
+package de.telran;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+ public class MathOperations {
+
+    public static void operator (int a, int b) {
+
+        int nums[] = new int[20];
+
+        int randomNum = ThreadLocalRandom.current().nextInt((a + 1), b);
+
+        if (randomNum < 1) {
+
+            System.out.println("please choose two integers with a greater range");
+
+
+        } else {
+            System.out.println(randomNum);
+        }
+        System.out.print("[");
+        for(int i =  0; i < nums.length; i++){
+
+            nums[i] = ThreadLocalRandom.current().nextInt((a + 1), b);
+            System.out.print(nums[i]);
+
+            if(i < (nums.length-1)){
+                System.out.print(", ");
+            }
+            else {
+                System.out.println("]");
+            }
+
+        }
+    }
+
+}
