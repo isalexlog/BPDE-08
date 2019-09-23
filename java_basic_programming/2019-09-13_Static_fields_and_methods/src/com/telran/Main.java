cbe8c0b199e9b35875c9f3daf9bc329bb7d92f73
lesyaAn <lisaalisa92@bk.ru>
2019-09-23 16:35:28 +0200

    Home Work 20.09.2019 Wrong Argument Exception

--- java_basic_programming/2019-09-13_Static_fields_and_methods/src/com/telran/Main.java
+++ java_basic_programming/2019-09-13_Static_fields_and_methods/src/com/telran/Main.java
@@ -1,42 +1,12 @@
 package com.telran;
 
 import java.lang.Math;
+import java.util.Scanner;
 
 import static com.telran.CircleUtils.*;
 
 public class Main {
 
-    /*
-    In com.telran.CircleUtils add validation into every method checking if radius
-    is not negative. If radius is negative, log error with with error method
-    of Logger class. and then return -1.
-
-    Create static method that
-    - receives two integer parameters a and b,
-    - returns random integer that is less then b and greater then a.
-    Fill with the method an array of 20 integers, and print array values
-    into the console into the one line.
-    Line must start with '[' and end with ']'. Elements in the line must
-    be separated with commas (',').
-
-    */
-
-    /*
-    HW 2019-09-20
-    1.Create WrongArgumentException class and throw this exception
-    if argument is not valid. Please extend RuntimeException and
-    use you own error message.
-
-    2. throw this exception in all CircleUtils Methods
-
-    3. Modify main in the following way:
-    - ask user if he wants to calculate circle length, circle area or square
-    equivalent, or he wants to exit
-    - ask user for the radius
-    - calculate the desired value
-    - use try catch to inform user that wrong argument was supplied
-
-     */
 
     public static void main(String[] args) {
         Logger log = Logger.getLogger(Main.class);
@@ -47,6 +17,31 @@ public class Main {
         log.info(squareEquivalent(1));
         log.info(Math.sqrt(PI));
 
-        log.info(squareEquivalent(-1));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your request: 1 - length, 2 - area, 3 - square equivalent, or any other way to exit");
+        int number = scanner.nextInt();
+        try {
+            switch (number) {
+                case 1:
+                    System.out.println("Please enter a radius: ");
+                    System.out.println(circleLength(scanner.nextDouble()));
+                    break;
+                case 2:
+                    System.out.println("Please enter a radius: ");
+                    System.out.println(circleArea(scanner.nextDouble()));
+                    break;
+                case 3:
+                    System.out.println("Please enter a radius: ");
+                    System.out.println(squareEquivalent(scanner.nextDouble()));
+                case 4:
+                    break;
+                default:
+                    System.out.println("Oops!Wrong argument, please try again!");
+            }
+        } catch (WrongArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
