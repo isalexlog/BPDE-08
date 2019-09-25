c049f4b1435a04a7f1893362b1493d2a4dc993be
Evgenygroup <mailfrom61@gmail.com>
2019-09-25 20:04:22 +0200

    New commit

--- java_basic_programming/2019-09-13_Static_fields_and_methods/src/com/telran/Main.java
+++ java_basic_programming/2019-09-13_Static_fields_and_methods/src/com/telran/Main.java
@@ -36,7 +36,7 @@ public class Main {
     - calculate the desired value
     - use try catch to inform user that wrong argument was supplied
 
-    //New comment just to commit
+    //New comment  to commit
 
      */
 
