0b792c7d1551f0dc6777a4f4ca09f43c7242c57f
Alex Loginov <alexander.loginov@takeaway.com>
2019-09-27 20:10:48 +0200

    ArrayList - our own implementation

---
+++ java_basic_programming/2019-09-27_ArrayList/src/de/telran/ArrayList.java
@@ -0,0 +1,35 @@
+package de.telran;
+
+public class ArrayList<T> {
+
+    private final int INITIAL_ARRAY_SIZE = 1000;
+
+    private int size = 0;
+
+    private T[] array;
+
+    public ArrayList() {
+        array = (T[]) new Object[INITIAL_ARRAY_SIZE];
+    }
+
+    public void add(T object) {
+        if (size + 1 > array.length) {
+            T[] newArray = (T[]) new Object[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+
+        array[size] = object;
+        size++;
+    }
+
+    public T get(int index) {
+        return array[index];
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
