695e9fb0c7e0c71a7749b2757604cc95af040d19
lesyaAn <lisaalisa92@bk.ru>
2019-10-01 19:29:42 +0200

    test

---
+++ java_basic_programming/2019-09-27_ArrayList/src/de/telran/ArrayListTest.java
@@ -0,0 +1,22 @@
+package de.telran;
+
+import static org.junit.Assert.*;
+
+public class ArrayListTest {
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void get() {
+    }
+
+    @org.junit.Test
+    public void getSize() {
+    }
+
+    @org.junit.Test
+    public void remove() {
+    }
+}
