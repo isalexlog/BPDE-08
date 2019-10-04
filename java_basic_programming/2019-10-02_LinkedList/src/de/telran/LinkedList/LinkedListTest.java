064cb479020b580a639762d75f0bf45f533ea12e
Evgenygroup <mailfrom61@gmail.com>
2019-10-04 09:24:01 +0200

    HW LinkedList methods

--- java_basic_programming/2019-10-02_LinkedList/src/de/telran/LinkedList/LinkedListTest.java
+++ java_basic_programming/2019-10-02_LinkedList/src/de/telran/LinkedList/LinkedListTest.java
@@ -1,5 +1,6 @@
 package de.telran.LinkedList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,11 +9,16 @@ public class LinkedListTest {
 
     private LinkedList<String> linkedList = new LinkedList<>();
 
-    @Test
-    public void add() {
+    @Before
+    public void init() {
         linkedList.add("First");
         linkedList.add("Second");
         linkedList.add("Third");
+
+    }
+
+    @Test
+    public void add() {
 
         assertEquals("First", linkedList.get(0));
         assertEquals("Second", linkedList.get(1));
@@ -20,10 +26,52 @@ public class LinkedListTest {
     }
 
     @Test
-    public void get() {
+    public void getFirst() {
+        assertEquals("First", linkedList.getFirst());
+
     }
 
     @Test
-    public void remove() {
+    public void getLast() {
+        assertEquals("Third", linkedList.getLast());
+
+
     }
+
+    @Test
+    public void getSize() {
+        assertEquals(3, linkedList.getSize());
+
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void removeLast() {
+        linkedList.remove(2);
+        linkedList.get(2);
+
+    }
+
+    @Test
+    public void remove0() {
+        linkedList.remove(0);
+        assertEquals("Second", linkedList.get(0));
+        assertEquals("Third", linkedList.get(1));
+
+    }
+
+    @Test
+    public void remove1() {
+        linkedList.remove(1);
+        assertEquals("First", linkedList.get(0));
+        assertEquals("Third", linkedList.get(1));
+
+    }
+
+    @Test
+    public void removeCompareSize() {
+        linkedList.remove(2);
+        assertEquals(2, linkedList.getSize());
+    }
+
 }
